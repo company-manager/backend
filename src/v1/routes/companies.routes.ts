66cffe6dac6976384f1797f1f5553089e475b5b9
Nuno <nunojllemos@gmail.com>
2024-03-02 11:34:31 +0000

    dev: add auth middleware to delete company route

--- src/v1/routes/companies.routes.ts
+++ src/v1/routes/companies.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import companyControllers from '@controllers-V1/companies.controllers'
+import { authenticateToken } from '@src/middleware/authorization'
 
 const router = Router()
 
 router.post('/', companyControllers.add)
 router.get('/:id', companyControllers.getById)
-router.delete('/:id', companyControllers.remove)
+
+// TODO: check if user who's trying to delete company has admin role
+router.delete('/:id', authenticateToken, companyControllers.remove)
 
 export default router
