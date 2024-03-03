b247132d4afe1ac83a1064b82e8ff58495a33912
Nuno <nunojllemos@gmail.com>
2024-03-03 22:53:43 +0000

    dev: creating clients routes

--- src/v1/controllers/clients.controller.ts
+++ src/v1/controllers/clients.controller.ts
@@ -1,10 +1,13 @@
+/* eslint-disable camelcase */
 import { Request, Response } from 'express'
 import clientsServices from '@services-V1/clients.services'
 import responses from '@src/helpers/responses'
 
-const getAll = async (req: Request, res: Response) => {
+const getAllByCompany = async (req: Request, res: Response) => {
     try {
-        const results = await clientsServices.getAll()
+        const { company_id } = req.cookies
+
+        const results = await clientsServices.getAllByCompany(company_id)
 
         if (!results) {
             return res.status(403).json({
@@ -21,13 +24,14 @@ const getAll = async (req: Request, res: Response) => {
 
 const getById = async (req: Request, res: Response) => {
     try {
-        const { id } = req.params
-        console.log(req.params)
-        const results = await clientsServices.getById(id)
+        const { id: clientId } = req.params
+        const { company_id } = req.cookies
 
-        if (!results) {
+        if (!company_id) {
             return res.status(400).json({ code: 400, message: 'Bad request' })
         }
+
+        const results = await clientsServices.getById(company_id, clientId)
 
         return res.status(200).json({ ...responses.ok, results })
     } catch (error) {
@@ -35,4 +39,44 @@ const getById = async (req: Request, res: Response) => {
     }
 }
 
-export default { getAll, getById }
+const remove = async (req: Request, res: Response) => {
+    try {
+        const { id: client_id } = req.params
+        const { company_id } = req.cookies
+
+        const doesClientExists = await clientsServices.getById(
+            company_id,
+            client_id,
+        )
+
+        if (!doesClientExists) {
+            return res.status(400).json({ ...responses.badRequest })
+        }
+
+        const results = await clientsServices.remove(company_id, client_id)
+
+        return res.status(200).json({ ...responses.ok, results })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const add = async (req: Request, res: Response) => {
+    try {
+        const { name, taxpayer_id } = req.body?.client
+
+        if (!!name || !!taxpayer_id) {
+            return res.status(400).json({ ...responses.badRequest })
+        }
+
+        const clientData = { name, taxpayer_id }
+
+        const results = clientsServices.add(clientData)
+
+        return res.status(200).json({ ...responses.ok, results })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default { getAllByCompany, getById, remove, add }
--- src/v1/repositories/clients.repository.ts
+++ src/v1/repositories/clients.repository.ts
@@ -1,14 +1,45 @@
+/* eslint-disable camelcase */
 import pool from '@database/index'
 import clientsQueries from '@queries-V1/clients.queries'
+
+type ClientData = {
+    name: string
+    taxpayer_id: string
+}
 
 const getAll = async () => {
     const results = await pool.query(clientsQueries.getAll)
     return results?.rows
 }
 
-const getById = async (id: string) => {
-    const results = await pool.query(clientsQueries.getById, [id])
+const getAllByCompany = async (companyId: string) => {
+    const results = await pool.query(clientsQueries.getManyByCompany, [
+        companyId,
+    ])
+    return results?.rows
+}
+
+const getById = async (companyId: string, clientId: string) => {
+    const results = await pool.query(clientsQueries.getOneByCompany, [
+        companyId,
+        clientId,
+    ])
+
     return results?.rows?.[0]
 }
 
-export default { getAll, getById }
+const remove = async (companyId: string, clientId: string) => {
+    const results = await pool.query(clientsQueries.remove, [
+        companyId,
+        clientId,
+    ])
+    return results?.rows?.[0]
+}
+
+const add = async (data: ClientData) => {
+    const { name, taxpayer_id } = data
+    const results = await pool.query(clientsQueries.add, [name, taxpayer_id])
+    return results
+}
+
+export default { getAll, getById, getAllByCompany, remove, add }
