68a8f2907e21a4a7983d0fbb0db093be90512fd2
nunojllemos <nunojllemos@gmail.com>
2024-03-18 18:51:50 +0000

    dev: finish client entity

--- src/v1/controllers/clients.controller.ts
+++ src/v1/controllers/clients.controller.ts
@@ -1,14 +1,16 @@
 /* eslint-disable camelcase */
+import responses from '@src/helpers/responses'
 import { Request, Response } from 'express'
 import clientsServices from '@services-V1/clients.services'
-import responses from '@src/helpers/responses'
 import { isEmpty } from '@utils/index'
 
-const getAllByCompany = async (req: Request, res: Response) => {
+// TODO handle errors
+
+const getAll = async (req: Request, res: Response) => {
     try {
         const { company_id } = req.cookies
 
-        const results = await clientsServices.getAllByCompany(company_id)
+        const results = await clientsServices.getAll(company_id)
 
         if (!results) {
             return res.status(403).json({
@@ -34,6 +36,13 @@ const getById = async (req: Request, res: Response) => {
 
         const results = await clientsServices.getById(company_id, clientId)
 
+        if (isEmpty(results)) {
+            return res.status(404).json({
+                ...responses.notFound,
+                message: `Id: ${clientId} not found`,
+            })
+        }
+
         return res.status(200).json({ ...responses.ok, results })
     } catch (error) {
         console.log(error)
@@ -42,19 +51,20 @@ const getById = async (req: Request, res: Response) => {
 
 const remove = async (req: Request, res: Response) => {
     try {
-        const { id: client_id } = req.params
+        const { id: clientId } = req.params
         const { company_id } = req.cookies
 
-        const doesClientExists = await clientsServices.getById(
-            company_id,
-            client_id,
-        )
+        const client = await clientsServices.getById(company_id, clientId)
+        const clientAlreadyExists = !isEmpty(client)
 
-        if (!doesClientExists) {
-            return res.status(400).json({ ...responses.badRequest })
+        if (!clientAlreadyExists) {
+            return res.status(404).json({
+                ...responses.notFound,
+                message: `Id: ${clientId} not found`,
+            })
         }
 
-        const results = await clientsServices.remove(company_id, client_id)
+        const results = await clientsServices.remove(company_id, clientId)
 
         return res.status(200).json({ ...responses.ok, results })
     } catch (error) {
@@ -64,27 +74,89 @@ const remove = async (req: Request, res: Response) => {
 
 const add = async (req: Request, res: Response) => {
     try {
-        const { name, taxpayer_id } = req.body?.client
+        const {
+            name,
+            taxpayer_id,
+            address_1,
+            address_2,
+            city,
+            country,
+            phone_1,
+            phone_2,
+        } = req.body?.client
         const { company_id } = req.cookies
 
-        if (isEmpty(name) || isEmpty(taxpayer_id)) {
-            return res.status(400).json({ ...responses.badRequest })
+        if (isEmpty(name) || isEmpty(company_id)) {
+            return res.status(400).json({
+                ...responses.badRequest,
+                message: 'No company id or client name given',
+            })
         }
 
-        const clientData = { name, taxpayer_id }
+        const client = await clientsServices.getByTaxpayerId(
+            company_id,
+            taxpayer_id,
+        )
+        const clientAlreadyExists = !isEmpty(client)
 
-        const results = await clientsServices.add(clientData, company_id)
-
-        if (results?.failed) {
-            return res
-                .status(400)
-                .json({ ...responses.badRequest, message: results.message })
+        if (clientAlreadyExists) {
+            return res.status(400).json({
+                ...responses.badRequest,
+                message: `Client with taxpayer id: ${taxpayer_id} already exists`,
+            })
         }
 
-        return res.status(200).json({ ...responses.ok, results })
+        const newClientData = {
+            name,
+            taxpayer_id,
+            address_1,
+            address_2,
+            city,
+            country,
+            phone_1,
+            phone_2,
+        }
+
+        const results = await clientsServices.add(newClientData, company_id)
+        return res.status(201).json({ ...responses.created, results })
     } catch (error) {
         console.log(error)
     }
 }
 
-export default { getAllByCompany, getById, remove, add }
+const update = async (req: Request, res: Response) => {
+    try {
+        const { id: clientId } = req.params
+        const { company_id } = req.cookies
+
+        if (!clientId && !company_id) {
+            return res.status(400).json({
+                ...responses.badRequest,
+                message: 'Client or company id missing',
+            })
+        }
+
+        const isRequestValid =
+            req.body.client && Object.keys(req.body.client)?.length > 0
+
+        if (!isRequestValid) {
+            return res
+                .status(400)
+                .json({ ...responses.badRequest, message: 'No data provided' })
+        }
+
+        const results = await clientsServices.update(
+            company_id,
+            clientId,
+            req.body.client,
+        )
+
+        return res
+            .status(200)
+            .json({ ...responses.ok, message: 'Client edited', results })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default { getAll, getById, remove, add, update }
