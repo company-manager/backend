ee7fbd74ea8b6520af5c8b2003370a2c9a811203
nunojllemos <nunojllemos@gmail.com>
2024-04-04 16:21:51 +0100

    dev: install and implement helmet

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { Express } from 'express'
+import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
 import cors from 'cors'
@@ -18,6 +19,7 @@ const app: Express = express()
 const PORT = process.env.PORT
 
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+app.use(helmet())
 app.use(express.json())
 app.use(cookieParser())
 
