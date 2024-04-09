e32a20d0775c0478ad7f22078184b35eab5b28bc
nunojllemos <nunojllemos@gmail.com>
2024-04-09 11:09:51 +0100

    dev: change projects service by projects repository in projects controller

--- src/v1/controllers/projects.controller.ts
+++ src/v1/controllers/projects.controller.ts
@@ -1,27 +1,27 @@
 /* eslint-disable camelcase */
 import { Request, Response } from 'express'
 import { Project } from '@global-types/index'
-import projectsServices from '@services-V1/projects.services'
+import projectsRepository from '@repositories-V1/projects.repository'
 import middlewareUtils from '@utils/middleware/.'
 
 const getAll = async (req: Request, res: Response) => {
-    middlewareUtils.doGetAll<Project>(req, res, projectsServices)
+    middlewareUtils.doGetAll<Project>(req, res, projectsRepository)
 }
 
 const getById = async (req: Request, res: Response) => {
-    middlewareUtils.doGetById<Project>(req, res, projectsServices)
+    middlewareUtils.doGetById<Project>(req, res, projectsRepository)
 }
 
 const create = async (req: Request, res: Response) => {
-    middlewareUtils.doCreate<Project, 'id'>(req, res, projectsServices)
+    middlewareUtils.doCreate<Project, 'id'>(req, res, projectsRepository)
 }
 
 const update = async (req: Request, res: Response) => {
-    middlewareUtils.doUpdate<Project>(req, res, projectsServices)
+    middlewareUtils.doUpdate<Project>(req, res, projectsRepository)
 }
 
 const remove = async (req: Request, res: Response) => {
-    middlewareUtils.doRemove<Project>(req, res, projectsServices)
+    middlewareUtils.doRemove<Project>(req, res, projectsRepository)
 }
 
 export default { getAll, getById, create, update, remove }
