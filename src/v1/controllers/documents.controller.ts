aa36567ed317ad0d5694607cdbe080f9a550b85d
nunojllemos <nunojllemos@gmail.com>
2024-04-09 11:11:13 +0100

    dev: change documents service by documents repository in documents controller

--- src/v1/controllers/documents.controller.ts
+++ src/v1/controllers/documents.controller.ts
@@ -1,30 +1,30 @@
 import { Request, Response } from 'express'
 import { Document } from '@global-types/index'
-import documentsServices from '@services-V1/documents.services'
+import documentsRepository from '@repositories-V1/documents.repository'
 import middlewareUtils from '@utils/middleware/.'
 
 const getAll = async (req: Request, res: Response) => {
-    middlewareUtils.doGetAll<Document>(req, res, documentsServices)
+    middlewareUtils.doGetAll<Document>(req, res, documentsRepository)
 }
 
 const getById = async (req: Request, res: Response) => {
-    middlewareUtils.doGetById<Document>(req, res, documentsServices)
+    middlewareUtils.doGetById<Document>(req, res, documentsRepository)
 }
 
 const create = async (req: Request, res: Response) => {
     middlewareUtils.doCreate<Document, 'id' | 'company_id'>(
         req,
         res,
-        documentsServices,
+        documentsRepository,
     )
 }
 
 const update = async (req: Request, res: Response) => {
-    middlewareUtils.doUpdate<Document>(req, res, documentsServices)
+    middlewareUtils.doUpdate<Document>(req, res, documentsRepository)
 }
 
 const remove = async (req: Request, res: Response) => {
-    middlewareUtils.doRemove<Document>(req, res, documentsServices)
+    middlewareUtils.doRemove<Document>(req, res, documentsRepository)
 }
 
 export default { getAll, getById, create, update, remove }
