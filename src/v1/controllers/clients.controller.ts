ac06321562b46c156d79f85c60cedac5f5eecf87
nunojllemos <nunojllemos@gmail.com>
2024-04-09 11:15:00 +0100

    dev: change clients service by clients repository in clients controller

--- src/v1/controllers/clients.controller.ts
+++ src/v1/controllers/clients.controller.ts
@@ -1,31 +1,31 @@
 /* eslint-disable camelcase */
 import { Request, Response } from 'express'
 import { Client } from '@global-types/index'
-import clientsServices from '@services-V1/clients.services'
+import clientsRepository from '@repositories-V1/clients.repository'
 import middlewareUtils from '@utils/middleware/.'
 
 const getAll = async (req: Request, res: Response) => {
-    middlewareUtils.doGetAll<Client>(req, res, clientsServices)
+    middlewareUtils.doGetAll<Client>(req, res, clientsRepository)
 }
 
 const getById = async (req: Request, res: Response) => {
-    middlewareUtils.doGetById<Client>(req, res, clientsServices)
+    middlewareUtils.doGetById<Client>(req, res, clientsRepository)
 }
 
 const create = async (req: Request, res: Response) => {
     middlewareUtils.doCreate<Client, 'id' | 'company_id'>(
         req,
         res,
-        clientsServices,
+        clientsRepository,
     )
 }
 
 const update = async (req: Request, res: Response) => {
-    middlewareUtils.doUpdate<Client>(req, res, clientsServices)
+    middlewareUtils.doUpdate<Client>(req, res, clientsRepository)
 }
 
 const remove = async (req: Request, res: Response) => {
-    middlewareUtils.doRemove<Client>(req, res, clientsServices)
+    middlewareUtils.doRemove<Client>(req, res, clientsRepository)
 }
 
 export default { getAll, getById, create, remove, update }
