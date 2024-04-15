51e6506ff6ccd7bc39864479662194265c286faf
nunojllemos <nunojllemos@gmail.com>
2024-04-15 11:09:45 +0100

    dev: changing cron job schedule

--- src/cron.ts
+++ src/cron.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 // import cron from 'node-cron'
 
-// const EVERY_SECOND = '* * * * * *'
-// const EVERY_MINUTE = '* * * * *'
+const EVERY_SECOND = '* * * * * *'
+const EVERY_MINUTE = '* * * * *'
+const EVERY_HOUR = '* 1 * * *'
 
-// const job = cron.schedule(EVERY_MINUTE, () => {
-//     console.log(`cron-job @ ${new Date().toLocaleString()}`)
-// })
+const job = cron.schedule(EVERY_HOUR, () => {
+    console.log(`cron-job @ ${new Date().toLocaleString()}`)
+})
 
 // export { job }
